a469bf7993319980802e0fac9cd010fd9c6d5ed9
HemangNakarani <hemangnh18@gmail.com>
2021-04-24 17:19:57 +0530

    Fixed Some Fields in models

--- src/main/java/io/sen/canteenia/models/OrderedItem.java
+++ src/main/java/io/sen/canteenia/models/OrderedItem.java
@@ -1,6 +1,11 @@
 package io.sen.canteenia.models;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Table(name = "orderitem")
@@ -20,7 +25,19 @@ public class OrderedItem {
 
     private int amount = 0;
 
+    private boolean paid;
+
     private String status;
+
+    @CreationTimestamp
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="IST")
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss", timezone="IST")
+    @Column(name = "updated_at", nullable = false)
+    private Date undatedAt;
 
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "order_fooditem", nullable = false)
@@ -102,5 +119,29 @@ public class OrderedItem {
 
     public void setCartfooditem(FoodItem cartfooditem) {
         this.cartfooditem = cartfooditem;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUndatedAt() {
+        return undatedAt;
+    }
+
+    public void setUndatedAt(Date undatedAt) {
+        this.undatedAt = undatedAt;
+    }
+
+    public boolean isPaid() {
+        return paid;
+    }
+
+    public void setPaid(boolean paid) {
+        this.paid = paid;
     }
 }
