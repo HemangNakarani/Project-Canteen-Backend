4983071cc6e73285d82352129f5bd6aeed7fa7c5
HemangNakarani <hemangnh18@gmail.com>
2021-05-02 06:52:18 +0530

    fix: reset password template

--- src/main/java/io/sen/canteenia/email/EmailUtils.java
+++ src/main/java/io/sen/canteenia/email/EmailUtils.java
@@ -9,143 +9,206 @@ public class EmailUtils {
     public String getResetPasswordTemplate(String email, String token)
     {
         String html = "<!doctype html>\n" +
-                "<html lang=\"en-US\">\n" +
+                "<html lang=\\en-US\\>\n" +
                 "\n" +
                 "<head>\n" +
-                "    <meta content=\"text/html; charset=utf-8\" http-equiv=\"Content-Type\" />\n" +
+                "    <meta content=\"text/html\"; charset=\"utf-8\" http-equiv=\"Content-Type\" />\n" +
                 "    <title>Reset Password Email Template</title>\n" +
-                "    <meta name=\"description\" content=\"Reset Password Email Template.\">\n" +
+                "    <meta name=\"description\" content=\\Reset Password Email Template.\\>\n" +
                 "    <style type=\"text/css\">\n" +
                 "        @import url(http://fonts.googleapis.com/css?family=Raleway:700,800);\n" +
                 "\n" +
-                "        html, body { margin: 0; }\n" +
+                "        html,\n" +
+                "        body {\n" +
+                "            margin: 0;\n" +
+                "        }\n" +
                 "\n" +
-                "        :focus { outline: none; }\n" +
-                "        ::-webkit-input-placeholder { color: #DEDFDF; }\n" +
-                "        ::-moz-placeholder { color: #DEDFDF; }\n" +
-                "        :-moz-placeholder { color: #DEDFDF; }\n" +
-                "        ::-ms-input-placeholder { color: #DEDFDF; }\n" +
+                "        :focus {\n" +
+                "            outline: none;\n" +
+                "        }\n" +
+                "\n" +
+                "        ::-webkit-input-placeholder {\n" +
+                "            color: #DEDFDF;\n" +
+                "        }\n" +
+                "\n" +
+                "        ::-moz-placeholder {\n" +
+                "            color: #DEDFDF;\n" +
+                "        }\n" +
+                "\n" +
+                "        :-moz-placeholder {\n" +
+                "            color: #DEDFDF;\n" +
+                "        }\n" +
+                "\n" +
+                "        ::-ms-input-placeholder {\n" +
+                "            color: #DEDFDF;\n" +
+                "        }\n" +
                 "\n" +
                 "        body {\n" +
-                "        background: #6ED0F6;\n" +
-                "        color: #fff;\n" +
-                "        font-family: 'Raleway', sans-serif;\n" +
-                "        -webkit-font-smoothing: antialiased;\n" +
+                "            background: #6ED0F6;\n" +
+                "            color: #fff;\n" +
+                "            font-family: 'Raleway', sans-serif;\n" +
+                "            -webkit-font-smoothing: antialiased;\n" +
                 "        }\n" +
                 "\n" +
-                "        #wrapper, label, #arrow, button span { transition: all .5s cubic-bezier(.6,0,.4,1); }\n" +
+                "        #wrapper,\n" +
+                "        label,\n" +
+                "        #arrow,\n" +
+                "        button span {\n" +
+                "            transition: all .5s cubic-bezier(.6, 0, .4, 1);\n" +
+                "        }\n" +
                 "\n" +
-                "        #wrapper { overflow: hidden; }\n" +
+                "        #wrapper {\n" +
+                "            overflow: hidden;\n" +
+                "        }\n" +
                 "\n" +
-                "        #signin:checked ~ #wrapper { height: 178px; }\n" +
-                "        #signin:checked ~ #wrapper #arrow { left: 32px; }\n" +
-                "        #signin:checked ~ button span { transform: translate3d(0,-72px,0); }\n" +
+                "        #signin:checked~#wrapper {\n" +
+                "            height: 178px;\n" +
+                "        }\n" +
                 "\n" +
-                "        #signup:checked ~ #wrapper { height: 262px; }\n" +
-                "        #signup:checked ~ #wrapper #arrow { left: 137px; }\n" +
-                "        #signup:checked ~ button span { transform: translate3d(0,-144px,0); }\n" +
+                "        #signin:checked~#wrapper #arrow {\n" +
+                "            left: 32px;\n" +
+                "        }\n" +
                 "\n" +
-                "        #reset:checked ~ #wrapper { height: 94px; }\n" +
-                "        #reset:checked ~ #wrapper #arrow { left: 404px; }\n" +
-                "        #reset:checked ~ button span { transform: translate3d(0,0,0); }\n" +
+                "        #signin:checked~button span {\n" +
+                "            transform: translate3d(0, -72px, 0);\n" +
+                "        }\n" +
+                "\n" +
+                "        #signup:checked~#wrapper {\n" +
+                "            height: 262px;\n" +
+                "        }\n" +
+                "\n" +
+                "        #signup:checked~#wrapper #arrow {\n" +
+                "            left: 137px;\n" +
+                "        }\n" +
+                "\n" +
+                "        #signup:checked~button span {\n" +
+                "            transform: translate3d(0, -144px, 0);\n" +
+                "        }\n" +
+                "\n" +
+                "        #reset:checked~#wrapper {\n" +
+                "            height: 94px;\n" +
+                "        }\n" +
+                "\n" +
+                "        #reset:checked~#wrapper #arrow {\n" +
+                "            left: 404px;\n" +
+                "        }\n" +
+                "\n" +
+                "        #reset:checked~button span {\n" +
+                "            transform: translate3d(0, 0, 0);\n" +
+                "        }\n" +
                 "\n" +
                 "        form {\n" +
-                "        width: 450px;\n" +
-                "        height: 370px;\n" +
-                "        margin: -185px -225px;\n" +
-                "        position: absolute;\n" +
-                "        left: 50%;\n" +
-                "        top: 50%;\n" +
+                "            width: 450px;\n" +
+                "            height: 370px;\n" +
+                "            margin: -185px -225px;\n" +
+                "            position: absolute;\n" +
+                "            left: 50%;\n" +
+                "            top: 50%;\n" +
                 "        }\n" +
                 "\n" +
-                "        input[type=radio] { display: none; }\n" +
+                "        input[type=radio] {\n" +
+                "            display: none;\n" +
+                "        }\n" +
                 "\n" +
                 "        label {\n" +
-                "        cursor: pointer;\n" +
-                "        display: inline-block;\n" +
-                "        font-size: 22px;\n" +
-                "        font-weight: 800;\n" +
-                "        opacity: .5;\n" +
-                "        margin-bottom: 30px;\n" +
-                "        text-transform: uppercase;\n" +
+                "            cursor: pointer;\n" +
+                "            display: inline-block;\n" +
+                "            font-size: 22px;\n" +
+                "            font-weight: 800;\n" +
+                "            opacity: .5;\n" +
+                "            margin-bottom: 30px;\n" +
+                "            text-transform: uppercase;\n" +
                 "        }\n" +
+                "\n" +
                 "        label:hover {\n" +
-                "        transition: all .3s cubic-bezier(.6,0,.4,1);\n" +
-                "        opacity: 1;\n" +
+                "            transition: all .3s cubic-bezier(.6, 0, .4, 1);\n" +
+                "            opacity: 1;\n" +
                 "        }\n" +
-                "        label[for=\"signin\"] { margin-right: 20px; }\n" +
-                "        label[for=\"reset\"] { float: right; }\n" +
-                "        input[type=radio]:checked + label { opacity: 1; }\n" +
+                "\n" +
+                "        label[for=\"signin\"] {\n" +
+                "            margin-right: 20px;\n" +
+                "        }\n" +
+                "\n" +
+                "        label[for=\"reset\"] {\n" +
+                "            float: right;\n" +
+                "        }\n" +
+                "\n" +
+                "        input[type=radio]:checked label {\n" +
+                "            opacity: 1;\n" +
+                "        }\n" +
                 "\n" +
                 "        input[type=text],\n" +
                 "        input[type=password] {\n" +
-                "        background: #fff;\n" +
-                "        border: none;\n" +
-                "        border-radius: 8px;\n" +
-                "        font-size: 27px;\n" +
-                "        font-family: 'Raleway', sans-serif;\n" +
-                "        height: 72px;\n" +
-                "        width: 99.5%;\n" +
-                "        margin-bottom: 10px;\n" +
-                "        opacity: 1;\n" +
-                "        text-indent: 20px;\n" +
-                "        transition: all .2s ease-in-out;\n" +
+                "            background: #fff;\n" +
+                "            border: none;\n" +
+                "            border-radius: 8px;\n" +
+                "            font-size: 27px;\n" +
+                "            font-family: 'Raleway', sans-serif;\n" +
+                "            height: 72px;\n" +
+                "            width: 99.5%;\n" +
+                "            margin-bottom: 10px;\n" +
+                "            opacity: 1;\n" +
+                "            text-indent: 20px;\n" +
+                "            transition: all .2s ease-in-out;\n" +
                 "        }\n" +
+                "\n" +
                 "        button {\n" +
-                "        background: #079BCF;\n" +
-                "        border: none;\n" +
-                "        border-radius: 8px;\n" +
-                "        color: #fff;\n" +
-                "        cursor: pointer;\n" +
-                "        font-family: 'Raleway', sans-serif;\n" +
-                "        font-size: 27px;\n" +
-                "        height: 72px;\n" +
-                "        width: 100%;\n" +
-                "        margin-bottom: 10px;\n" +
-                "        overflow: hidden;\n" +
-                "        transition: all .3s cubic-bezier(.6,0,.4,1);\n" +
+                "            background: #079BCF;\n" +
+                "            border: none;\n" +
+                "            border-radius: 8px;\n" +
+                "            color: #fff;\n" +
+                "            cursor: pointer;\n" +
+                "            font-family: 'Raleway', sans-serif;\n" +
+                "            font-size: 27px;\n" +
+                "            height: 72px;\n" +
+                "            width: 100%;\n" +
+                "            margin-bottom: 10px;\n" +
+                "            overflow: hidden;\n" +
+                "            transition: all .3s cubic-bezier(.6, 0, .4, 1);\n" +
                 "        }\n" +
+                "\n" +
                 "        button span {\n" +
-                "        display: block;\n" +
-                "        line-height: 72px;\n" +
-                "        position: relative;\n" +
-                "        top: -2px;\n" +
-                "        transform: translate3d(0,0,0);\n" +
+                "            display: block;\n" +
+                "            line-height: 72px;\n" +
+                "            position: relative;\n" +
+                "            top: -2px;\n" +
+                "            transform: translate3d(0, 0, 0);\n" +
                 "        }\n" +
+                "\n" +
                 "        button:hover {\n" +
-                "        background: #007BA5;\n" +
+                "            background: #007BA5;\n" +
                 "        }\n" +
                 "\n" +
                 "        #arrow {\n" +
-                "        height: 0;\n" +
-                "        width: 0;\n" +
-                "        border-bottom: 10px solid #fff;\n" +
-                "        border-left: 10px solid transparent;\n" +
-                "        border-right: 10px solid transparent;\n" +
-                "        position: relative;\n" +
-                "        left: 32px;\n" +
+                "            height: 0;\n" +
+                "            width: 0;\n" +
+                "            border-bottom: 10px solid #fff;\n" +
+                "            border-left: 10px solid transparent;\n" +
+                "            border-right: 10px solid transparent;\n" +
+                "            position: relative;\n" +
+                "            left: 32px;\n" +
                 "        }\n" +
                 "\n" +
                 "\n" +
                 "        #hint {\n" +
-                "        width: 100%;\n" +
-                "        text-align: center;\n" +
-                "        position: absolute;\n" +
-                "        bottom: 20px;\n" +
+                "            width: 100%;\n" +
+                "            text-align: center;\n" +
+                "            position: absolute;\n" +
+                "            bottom: 20px;\n" +
                 "        }\n" +
                 "    </style>\n" +
                 "</head>\n" +
                 "\n" +
                 "<body>\n" +
                 "    <form id='password-form'>\n" +
-                "      <div id=\"wrapper\">\n" +
-                "        \n" +
-                "        <input id=\"password\" placeholder=\"New Password\" type=\"password\">\n" +
-                "        <input id=\"password1\" placeholder=\"Confirm password\" type=\"password\">\n" +
-                "      </div>\n" +
-                "      <button type=\"submit\" value=\"submit\">\n" +
-                "          Reset password\n" +
-                "      </button>\n" +
+                "        <div id=\"wrapper\">\n" +
+                "\n" +
+                "            <input id=\"password\" placeholder=\"New Password\" type=\"password\">\n" +
+                "            <input id=\"password1\" placeholder=\"Confirm password\" type=\"password\">\n" +
+                "        </div>\n" +
+                "        <button type=\"submit\" value=\"submit\">\n" +
+                "            Reset password\n" +
+                "        </button>\n" +
                 "    </form>\n" +
                 "    <div id=\"hint\">Powered By @ McDA's</div>\n" +
                 "</body>\n" +
@@ -157,33 +220,36 @@ public class EmailUtils {
                 "        const password = document.getElementById('password').value;\n" +
                 "        const password2 = document.getElementById('password1').value;\n" +
                 "\n" +
-                "        if(password !== password2){\n" +
-                "                alert('Passwords do not match,Please try again');\n" +
-                "                return;\n" +
+                "        document.getElementById('password').value = \"\";\n" +
+                "        document.getElementById('password1').value = \"\";\n" +
+                "\n" +
+                "        if (password !== password2) {\n" +
+                "            alert('Passwords do not match,Please try again');\n" +
+                "            return;\n" +
                 "        }\n" +
                 "\n" +
-                "        var res = await fetch('/api/auth/reset-password',{\n" +
+                "        var res = await fetch('/api/auth/reset-password', {\n" +
                 "            method: 'POST',\n" +
                 "            headers: {\n" +
-                "                'Content-Type':'application/json'\n" +
+                "                'Content-Type': 'application/json'\n" +
                 "            },\n" +
                 "            body: JSON.stringify({\n" +
-                "                plainUserPassword:password,\n" +
+                "                plainUserPassword: password,\n" +
                 "                email:\""+ email +"\",\n" +
                 "                token:\""+ token +"\"\n" +
                 "            })\n" +
                 "        });\n" +
                 "        var resJson = await res.json();\n" +
                 "        console.log(resJson);\n" +
-                "        if(resJson.status === 'ok'){\n" +
-                "            console.log(\"Password changed successfully!\");\n" +
+                "        if (resJson.status === 'ok') {\n" +
+                "            alert(\"Password changed successfully!\")\n" +
                 "        }\n" +
-                "        else{\n" +
-                "            console.log(\"An error has occured while changing password!!\");\n" +
+                "        else {\n" +
+                "            alert(resJson.status);\n" +
                 "        }\n" +
                 "    }\n" +
                 "    const form = document.getElementById('password-form');\n" +
-                "    form.addEventListener('submit',resetPasssword);\n" +
+                "    form.addEventListener('submit', resetPasssword);\n" +
                 "</script>\n" +
                 "\n" +
                 "</html>";
